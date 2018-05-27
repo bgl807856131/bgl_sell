56dcb51505c52938e4b0aadf69c733d1228a37a2
bgl <bu_gl@gzhc365.com>
2018-05-27 21:50:15 +0800

    添加转换器

---
+++ src/main/java/com/sell/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,42 @@
+package com.sell.converter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.sell.common.ResultEnum;
+import com.sell.dataobject.OrderDetail;
+import com.sell.dto.OrderDTO;
+import com.sell.exception.SellException;
+import com.sell.form.OrderForm;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by buguoliang on 2018/4/8.
+ */
+@Slf4j
+public class OrderForm2OrderDTOConverter {
+
+    public static OrderDTO converter(OrderForm orderForm) {
+        Gson gson = new Gson();
+
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setBuyerPhone(orderForm.getPhone());
+        orderDTO.setBuyerOpenid(orderForm.getOpenid());
+
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        try {
+            orderDetailList = gson.fromJson(orderForm.getItems(),
+                    new TypeToken<List<OrderDetail>>(){}.getType());
+        } catch (Exception e) {
+            log.error("【对象转换】错误，string = {}", orderForm.getItems());
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+}
